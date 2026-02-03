104d45b9580a0833e656a06607a80775550f4925
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-02-02 20:49:18 -0600

    Add first migration

---
+++ TechNotes.Infrastructure/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TechNotes.Infrastructure;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TechNotes;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False",
+         b => b.MigrationsAssembly("TechNotes.Infrastructure"));
+
+         return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
