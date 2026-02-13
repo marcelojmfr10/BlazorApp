0da026c463704eb783de92bab877a3ee186a7b82
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-02-12 19:16:31 -0600

    Se agregó la entidad Usuario y los comandos para login y registro

---
+++ TechNotes.Application/Authentication/RegisterUserResponse.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TechNotes.Application.Authentication;
+
+public class RegisterUserResponse
+{
+    public bool Succeeded { get; set; }
+    public List<string> Errors { get; set; } = [];
+}
--- TechNotes.Infrastructure/DependencyInjection.cs
+++ TechNotes.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TechNotes.Application.Authentication;
 using TechNotes.Domain.Notes;
+using TechNotes.Infrastructure.Authentication;
 using TechNotes.Infrastructure.Repositories;
 
 namespace TechNotes.Infrastructure;
@@ -10,10 +15,27 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=DefaultConnection", b => b.MigrationsAssembly("TechNotes.Infrastructure")));
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer("name=DefaultConnection", b => b.MigrationsAssembly("TechNotes.Infrastructure")));
 
         services.AddScoped<INoteRepository, NoteRepository>();
+        AddAuthentication(services);
 
         return services;
+    }
+
+    private static void AddAuthentication(IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+        services.AddCascadingAuthenticationState();
+        services.AddAuthorization();
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = IdentityConstants.ApplicationScheme;
+            options.DefaultChallengeScheme = IdentityConstants.ExternalScheme;
+        }).AddIdentityCookies();
+        services.AddIdentityCore<User>()
+        .AddEntityFrameworkStores<ApplicationDbContext>().AddSignInManager().AddDefaultTokenProviders();
     }
 }
